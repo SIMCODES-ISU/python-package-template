7bfa7e76da0760e0e4c322dddcf1d4c8c991a8da
zachcran <15938371+zachcran@users.noreply.github.com>
2026-05-27 18:02:44 -0600

    Move hello_world() function to its own submodule, 'hello.py'

---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/hello.py
@@ -0,0 +1,11 @@
+"""Provides an implementation of the classic Hello World function."""
+
+
+def hello_world() -> str:
+    """A simple function that returns 'Hello World!'.
+
+    :returns: String containing 'Hello World!'
+    :rtype: str
+    """
+
+    return "Hello World!"
