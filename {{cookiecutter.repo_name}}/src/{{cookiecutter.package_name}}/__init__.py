0140391f0e15623f1c0210b41547150fda24a953
zachcran <15938371+zachcran@users.noreply.github.com>
2026-05-27 18:19:13 -0600

    Add basic CLI app that calls hello_world() and can print its current version

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/__init__.py
@@ -2,7 +2,7 @@ from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
 # Dynamically fetch version of the package
 try:
-    # Replace '__package--' if pyproject.toml:project.name and the package
+    # Replace '__package__' if pyproject.toml:project.name and the package
     # directory under src/ are renamed to no longer match
     __version__ = version(__package__)
 except PackageNotFoundError:  # pragma: no cover
---
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.package_name}}/apps/main_cli.py
@@ -0,0 +1,65 @@
+import argparse
+import logging
+import sys
+
+from .. import __version__
+from ..hello import hello_world
+
+logger = logging.getLogger(__name__)
+
+
+def parse_args(args: list[str]) -> argparse.Namespace:
+    """Parses all command line arguments.
+
+    :param args: Command line arguments to be parsed.
+    :type args: list[str]
+    :return: Parsed command line arguments
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(description="Example CLI program")
+
+    # Positional arguments
+    # parser.add_argument("file", nargs="?")
+
+    # Optional flags
+    parser.add_argument(
+        "--version",
+        action="store_true",
+        help="Output version information and exit.",
+    )
+
+    return parser.parse_args()
+
+
+def main(argv: list[str]) -> int:
+    """Entry point of the program.
+
+    :param argv: Command line argument values.
+    :type argv: list[str]
+    :return: Return code; should use values in range [0, 255]
+    :rtype: int
+    """
+    args = parse_args(argv)
+
+    # Print version and exit
+    if args.version:
+        print(f"{__version__}")
+        return 0
+
+    hello_world()
+
+
+def run() -> int:
+    """Entry point of program that provides command line arguments.
+
+    As opposed to ``main()``, this function is suitable for use in
+    ``pyproject.toml:project.scripts`` entries since it has no parameters.
+
+    :return: return code; should use values in range [0, 255]
+    :rtype: int
+    """
+    return main(sys.argv[1:])
+
+
+if __name__ == "__main__":
+    sys.exit(run())
